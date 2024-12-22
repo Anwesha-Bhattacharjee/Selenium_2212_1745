a16b8a5a07f67847bd039684837e5417e2a912c0
Anwesha-Bhattacharjee <mohor122@gmail.com>
2024-12-22 18:06:12 +0530

    Init Commit

---
+++ src/main/java/com/avi/Selenium_2212_1745/App.java
@@ -0,0 +1,10 @@
+package com.avi.Selenium_2212_1745;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/avi/Selenium_2212_1745/LaunchBrowser.java
@@ -0,0 +1,26 @@
+package com.avi.Selenium_2212_1745;
+
+//import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class LaunchBrowser {
+
+    /**
+     * Rigorous Test :-)
+     */
+	@Test
+    public void shouldAnswerWithTrue() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertEquals(driver.getTitle(), "Google");
+        driver.quit();
+        	
+    }
+}
